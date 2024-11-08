cd0ed924dccc05bf4bf5c1ece9d4ad3d37e746a2
Econan <151092188+econanringo@users.noreply.github.com>
2024-11-08 05:11:43 +0000

    fixed navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
     <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav className="fixed top-0 left-0 w-full bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 z-50">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="/" className="flex items-center">
                     <img src="logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
